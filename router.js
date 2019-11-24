e02561b0edce007e22ede150eb7f50b927b0815b
tryptamine25 <johnym.25@gmail.com>
2019-11-24 19:58:24 +0300

    add test component

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Index from "./src/components/Index"
 import Polls from "./src/components/Polls"
 import Admin from "./src/components/Admin"
+import Test from "./src/components/Test"
 
 Vue.use(Router);
 
@@ -25,6 +26,12 @@ export default new Router({
             path: '/admin/',
             name: 'admin',
             component: Admin,
+            props: true,
+        },
+        {
+            path: '/test/',
+            name: 'test',
+            component: Test,
             props: true,
         }
     ]
--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,18 @@
         <router-link to="/" style="text-decoration: none; color: white">
           <span>POLL</span>
         </router-link>
-        <span class="font-weight-light">s</span>
+        <span class="font-weight-light">s </span>
         <router-link to="/polls" style="text-decoration: none; color: white">
-          <span>polls</span>
+          <span>polls </span>
         </router-link>
         <router-link to="/admin" style="text-decoration: none; color: white">
-          <span>admin</span>
+          <span>admin </span>
+        </router-link>
+        <router-link to="/test" style="text-decoration: none; color: white">
+          <span>test </span>
         </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <span class="mr-2">
-        <login-form></login-form>
-      </span>
     </v-app-bar>
 
     <v-content>
