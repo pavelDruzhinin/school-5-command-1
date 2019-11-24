ddd1b4c72b1cf68602a905aa6b6d1b4b1e40f5d3
tryptamine25 <johnym.25@gmail.com>
2019-11-24 16:22:22 +0300

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-footer
+            dense fixed app
+            padless
+    >
+        <v-row
+                justify="center"
+                no-gutters
+        >
+            <v-btn
+                    v-for="link in links"
+                    :key="link"
+                    color="white"
+                    text
+                    rounded
+                    class="my-2"
+            >
+                {{ link }}
+            </v-btn>
+            <v-col
+                    class="py-4 text-center white--text"
+                    cols="12"
+            >
+                {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+            </v-col>
+        </v-row>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            links: [
+                'Home',
+                'About Us',
+                'Team',
+                'Services',
+                'Blog',
+                'Contact Us',
+            ],
+        }),
+    }
+</script>
