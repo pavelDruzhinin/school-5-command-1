2606f00674eece9aefcb73947bc5089ac7de57d5
antiwinforever <antiwinforever@gmail.com>
2019-11-24 23:43:56 +0300

    Add DbContext

---
+++ backend/src/Answers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chatbot
+{
+    public class Answers
+    {
+        int Id { get; set; }
+        int IdUser { get; set; }
+        int IdQuestion { get; set; }
+        string Answer { get; set; }
+        DateTime DateBegin { get; set; }
+        DateTime DateEnd { get; set; }
+    }
+}
---
+++ backend/src/ApplicationDBcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chatbot
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Answers> Users { get; set; }
+    }
+}
