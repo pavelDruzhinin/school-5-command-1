1deb393c77e1467a6b5eb74c94954407a4a341b6
Andromsikk <igolkaman@gmail.com>
2019-11-26 00:04:54 +0300

    Работа с Polls.vue; Добавлены карточки(пока без адаптива

--- src/components/Polls.vue
+++ src/components/Polls.vue
@@ -1,38 +1,204 @@
 <template>
-    <v-card
-            class="mx-auto"
-            max-width="400"
-    >
-        <v-img
-                class="white--text align-end"
-                height="200px"
-                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    <div class="d-flex flex-row flex-wrap justify-space-around">
+
+
+
+
+
+  
+
+        <section class="ma-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://avatars.mds.yandex.net/get-pdb/1626505/accb297d-7c01-405e-a97d-82c4c5abbadf/s1200?webp=false"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Yandex.Inc</v-card-subtitle>
+
+                    <v-card-text >
+                        <div><h3 class="text--primary">Yandex Academic Supervision Program</h3></div>
+                        <div class="pb-0">Moscow &#183; Python, C++ &#183; Specialist</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section>  
+            
+            
+            
+        <section class="ma-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://zhiznonline.ru/wp-content/uploads/2019/01/1-1142.jpg"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Google.Inc</v-card-subtitle>
+
+                    <v-card-text >
+                        <div><h3 class="text--primary">UX Engineer, Front End, iOS</h3></div>
+                        <div class="pb-0">San Francisco &#183; Objective-C, Swif&#183; Specialist</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section>  
+            
+
+
+        <section class="ma-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://pbs.twimg.com/media/D3dNX6zWkAAG2CH.jpg:large"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Tesla.Inc</v-card-subtitle>
+
+                    <v-card-text >
+                        <div><h3 class="text--primary">Senior Passive Safety Test Engineer</h3></div>
+                        <div class="pb-0">Palo Alto &#183; Microelectronics &#183; Senior</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section>  
+            
+
+            
+        
+        <section class="ma-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://pbs.twimg.com/media/Dsk_StcWsAA7V0u.jpg:large"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Amazon</v-card-subtitle>
+
+                    <v-card-text >
+                        <div><h3 class="text--primary">Specialized Technical Account Manager </h3></div>
+                        <div class="pb-0">New York &#183; Manager, media  &#183; Specialist</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section>  
+            
+
+
+
+        <!--<v-card
+                class="mx-auto mx-10"
+                max-width="400"
         >
-            <v-card-title>Top 10 Australian beaches</v-card-title>
-        </v-img>
-
-        <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-        <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
-        </v-card-text>
-
-        <v-card-actions>
-            <v-btn
-                    color="orange"
-                    text
+            <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://avatars.mds.yandex.net/get-pdb/1626505/accb297d-7c01-405e-a97d-82c4c5abbadf/s1200?webp=false"
             >
-                Share
-            </v-btn>
+             <v-card-title>Top 10 Australian beaches</v-card-title>
+            </v-img>
 
-            <v-btn
-                    color="orange"
-                    text
-            >
-                Explore
-            </v-btn>
-        </v-card-actions>
-    </v-card>
+            <v-card-subtitle class="pb-0">Yandex.Inc</v-card-subtitle>
+
+            <v-card-text >
+                <div><h3 class="text--primary">Yandex Academic Supervision Program</h3></div>
+                <div class="pb-0">Moscow &#183; Python, C++ &#183; Specialist</div>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-btn
+                        color="cyan lighten-1"
+                        text
+                >
+                    begin
+                </v-btn>
+            </v-card-actions>
+        </v-card>-->
+
+
+
+
+
+
+
+
+
+
+    </div>
+    
 </template>
