01d243058be5ff71c28ceffc16ff8c4b6206ca3c
Andromsikk <igolkaman@gmail.com>
2019-11-26 20:48:17 +0300

    Сделано 8 карточек с опросами. Адаптивность присутствует. Работа с Polls.vue

--- src/components/Polls.vue
+++ src/components/Polls.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="d-flex flex-row flex-wrap justify-space-around">
+    <div class="d-flex flex-row flex-wrap justify-center">
 
 
 
 
 
   
-
-        <section class="ma-10">
+            <!--Карточка1 - Яндекс-->
+        <section class="ma-5 mt-10">
         <v-hover>
                 <template v-slot="{ hover }">
                 <v-card
-                        class="mx-auto p-3"
+                        class=" p-3"
                         max-width="400"
                         :elevation="hover ? 24 : 1"    
                 >
@@ -25,9 +25,9 @@
 
                     <v-card-subtitle class="pb-0">Yandex.Inc</v-card-subtitle>
 
-                    <v-card-text >
-                        <div><h3 class="text--primary">Yandex Academic Supervision Program</h3></div>
-                        <div class="pb-0">Moscow &#183; Python, C++ &#183; Specialist</div>
+                    <v-card-text class=" pb-0">
+                        <div ><h3 class="text--primary">Yandex Academic Supervision Program</h3></div>
+                        <div >Moscow &#183; Python, C++ &#183; Specialist</div>
                     </v-card-text>
 
                     <v-card-actions>
@@ -44,8 +44,8 @@
         </section>  
             
             
-            
-        <section class="ma-10">
+            <!--Карточка2 -Гугл-->
+        <section class="ma-5 mt-10">
         <v-hover>
                 <template v-slot="{ hover }">
                 <v-card
@@ -63,7 +63,7 @@
 
                     <v-card-subtitle class="pb-0">Google.Inc</v-card-subtitle>
 
-                    <v-card-text >
+                    <v-card-text class=" pb-0">
                         <div><h3 class="text--primary">UX Engineer, Front End, iOS</h3></div>
                         <div class="pb-0">San Francisco &#183; Objective-C, Swif&#183; Specialist</div>
                     </v-card-text>
@@ -82,8 +82,8 @@
         </section>  
             
 
-
-        <section class="ma-10">
+            <!--Карточка3 - Тесла-->
+        <section class="ma-5 mt-10">
         <v-hover>
                 <template v-slot="{ hover }">
                 <v-card
@@ -101,7 +101,7 @@
 
                     <v-card-subtitle class="pb-0">Tesla.Inc</v-card-subtitle>
 
-                    <v-card-text >
+                    <v-card-text class=" pb-0">
                         <div><h3 class="text--primary">Senior Passive Safety Test Engineer</h3></div>
                         <div class="pb-0">Palo Alto &#183; Microelectronics &#183; Senior</div>
                     </v-card-text>
@@ -121,8 +121,8 @@
             
 
             
-        
-        <section class="ma-10">
+            <!--Карточка4 - Амазон-->
+        <section class="ma-5 mt-10">
         <v-hover>
                 <template v-slot="{ hover }">
                 <v-card
@@ -140,7 +140,7 @@
 
                     <v-card-subtitle class="pb-0">Amazon</v-card-subtitle>
 
-                    <v-card-text >
+                    <v-card-text class=" pb-0">
                         <div><h3 class="text--primary">Specialized Technical Account Manager </h3></div>
                         <div class="pb-0">New York &#183; Manager, media  &#183; Specialist</div>
                     </v-card-text>
@@ -158,6 +158,164 @@
             </v-hover>
         </section>  
             
+            <!--Карточка5 - Близзард-->
+         <section class="ma-5 mt-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                            
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://finalweapon.net/wp-content/uploads/2019/07/Blizzard-KV.jpg"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Blizzard Entertainment</v-card-subtitle>
+
+                    <v-card-text class=" pb-0">
+                        <div><h3 class="text--primary">Senior Software Engineer, Gameplay</h3></div>
+                        <div class="pb-0">California &#183; C++, 5 years experience  &#183; Professional</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section>  
+
+
+            <!--Карточка6 - Близзард-->
+        <section class="ma-5 mt-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                            
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://img3.goodfon.ru/original/1920x1200/b/2b/intel-logo-intel-logotip-3579.jpg"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Intel</v-card-subtitle>
+
+                    <v-card-text class=" pb-0">
+                        <div><h3 class="text--primary">CPU pre-silicon verification Engineer</h3></div>
+                        <div class="pb-0">Santa Clara &#183;  Degree in Engineering,Software  &#183; Engineering</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section> 
+
+
+
+
+          <!--Карточка7 - Dvp.soft-->
+         <section class="ma-5 mt-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                        
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://sun9-3.userapi.com/c858432/v858432587/5123b/0s0W9LhXWs8.jpg"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Dvp.soft</v-card-subtitle>
+
+                    <v-card-text class=" pb-0">
+                        <div><h3 class="text--primary">Senior Software Engineer, Gameplay</h3></div>
+                        <div class="pb-0">Petrozavodsk &#183; Html, Css, Vue.js, React  &#183; Junior</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section>  
+
+
+
+ <!--Карточка7 - Dvp.soft-->
+         <section class="ma-5 mt-10">
+        <v-hover>
+                <template v-slot="{ hover }">
+                <v-card
+                        class="mx-auto p-3"
+                        max-width="400"
+                        
+                        :elevation="hover ? 24 : 1"    
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="200px"
+                            src="https://www.belekomahaber.com/wp-content/uploads/2018/08/bilecik1.jpg"
+                    >
+                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">Dvp.soft</v-card-subtitle>
+
+                    <v-card-text class=" pb-0">
+                        <div><h3 class="text--primary">Developer C++ (Mobile, KasperskyOS)</h3></div>
+                        <div class="pb-0">Sankt-Peterburg  &#183; C/C++, Posix.API  &#183; Senior</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                color="cyan lighten-1"
+                                text
+                        >
+                            begin
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </template>
+            </v-hover>
+        </section>  
+
 
 
 
